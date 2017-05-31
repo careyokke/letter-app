5d0773603ae9375667c19da5ba47e5645ed8c5ec
Anna Kuznetsova, Esvethlynna Pangelinan and Yokke Hargono <yokke@>
2017-05-31 15:43:02 -0700

    Letter application

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,8 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var name = $("input#name").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    $(".name").text(name);
 
     $("#story").show();
 
